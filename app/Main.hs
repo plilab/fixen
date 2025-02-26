e17d057817a29f1e50119d8468ce84c697614645
Luka Janjić <luka.janjic@gmail.com>
2025-02-26 13:40:43 +0800

    move the test from app to test

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Main where
+module Main (main) where
 
 import Prettyprinter
-import Parsing (parseTopLevel)
+import Parsing (declaration)
 
 main :: IO ()
-main = do
-  let res = parseTopLevel "\
-    \lat Dist\n\
-    \rel edge : Nat, Nat, Dist\n\
-    \rel distTo : Nat, Dist\n\
-    \rule init: |- distTo start 0\n\
-    \rule addDist: distTo v1 d1, edge v1 v2 d2, leq (add d1 d2) d |- distTo v2 d\n\
-    \ord: leq d11 d12 |- addDist {d1 = d11} <= addDist {d1 = d12}\n\
-    \query distTo as distTo - +\n"
-  case res of
-    Right prog -> print $ pretty prog
-    Left err -> print err
+main = putStrLn "repl to be implemented"
---
+++ test/Spec.hs
@@ -0,0 +1,17 @@
+import Prettyprinter
+import Prettyprinter.Render.Text
+import Parsing (parseTopLevel)
+
+main :: IO ()
+main = do 
+  let res = parseTopLevel "\
+    \lat Dist\n\
+    \rel edge : Nat, Nat, Dist\n\
+    \rel distTo : Nat, Dist\n\
+    \rule init: |- distTo start 0\n\
+    \rule addDist: distTo v1 d1, edge v1 v2 d2, leq (add d1 d2) d |- distTo v2 d\n\
+    \ord: leq d11 d12 |- addDist {d1 = d11} <= addDist {d1 = d12}\n\
+    \query distTo as distTo - +\n"
+  case res of
+    Right prog -> putDoc $ pretty prog
+    Left err -> print err
