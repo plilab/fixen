2572a2a50ab97bc781d5fa26b3b3febad1de44c2
Luka Janjić <luka.janjic@gmail.com>
2025-05-15 16:54:39 +0800

    fix a bug in compactify; introduce swapping

--- src/Compiler/Compactify.hs
+++ src/Compiler/Compactify.hs
@@ -35,20 +35,37 @@ unrollRule (UnordRule prems concl) =
 factorPremise :: Assumption Identifier -> [UnordRule] -> (Assumption Identifier, [UnordRule])
 factorPremise commonPremise rules =
   let
-    factor = mapMaybe $ \(UnordRule prems c) -> do
-      -- find an alpha equivalent premise
-      premise' <- find (alphaEq commonPremise) prems
+    factored = rules >>= \(UnordRule prems c) -> do
+      -- find all alpha equivalent premises
+      premise' <- filter (alphaEq commonPremise) . S.toList $ prems
       let
       -- remove it from the rule
         prems' = S.delete premise' prems
-        -- build a rename mapping:
-        renaming = M.fromList $ zip (idArgs premise') (idArgs commonPremise)
-          where idArgs = mapMaybe getId . argumentsOf
+        -- build a rename mapping
+        renaming =
+          M.fromList $ 
+            {- the renaming of old names to new names ensures no name 
+               clashing when we have multiple matching premises in one rule
+               e.g.
+                  rule: P a b, Q a, Q b |- R b
+                  commonPremise: Q a
+                  factoring (correct):
+                    Q a |- P a b |- Q b |- R b
+                        |- P b a |- Q a |- R a
+                  without the wapping the factoring would become:
+                    Q a |- P a b |- Q b |- R b
+                        |- P a a |- Q a |- R a
+            -}
+            zip oldNames newNames ++ zip newNames oldNames 
+          where
+            oldNames = idArgs premise'
+            newNames = idArgs commonPremise
+            idArgs = mapMaybe getId . argumentsOf
         -- rename rule's variables such that the premise becomes equal to `commonPremise`
         prems'' = S.map (substituteAll renaming) prems'
         c' = substituteAll renaming c
       return $ UnordRule prems'' c'
-  in (commonPremise, factor rules)
+  in (commonPremise, factored)
 
 {-
   per rule collect all unique premises up to _alpha_ equality
