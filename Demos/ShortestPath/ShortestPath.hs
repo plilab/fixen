09f316e3aefa0d6223bb6855fd1672a79fc14e91
skcheongbrian <skcheongbrian@gmail.com>
2025-08-07 23:47:29 +0800

    add data

--- Demos/ShortestPath/ShortestPath.hs
+++ Demos/ShortestPath/ShortestPath.hs
@@ -9,6 +9,7 @@ import Data.Bifunctor (Bifunctor(first))
 import Data.Foldable (Foldable(foldl'))
 import Data.Hashable
 import qualified Data.HashSet as S
+import qualified Data.HashMap.Strict as M
 import qualified Data.PQueue.Max as Q
 import GHC.Generics (Generic)
 import Numeric.Natural
@@ -53,18 +54,28 @@ data Fact = StartFact Start
           | EdgeFact Edge
               deriving (Show, Eq)
 
-instance Ord Fact where
-        (<=) (StartFact x) (StartFact y) = leq x y
-        (<=) (DistToFact x) (DistToFact y) = leq x y
-        (<=) (EdgeFact x) (EdgeFact y) = leq x y
+data Continuation = Initial Fact
+                  | AddDistCont String Dist String Dist
+                  | InitCont String
+                      deriving (Show, Eq)
+
+evaluate :: DataBase -> Continuation -> [Fact]
+evaluate _ (Initial f) = [f]
+evaluate db (AddDistCont v10 d10 v20 d20)
+  = [DistToFact (DistTo v20 (add d10 d20))]
+evaluate db (InitCont s0) = [DistToFact (DistTo s0 (DistNat 0))]
+
+instance Ord Continuation where
+        (<=) _ (Initial _) = True
         (<=) _ _ = False
 
-data DataBase = DataBase{factsStart :: S.HashSet Start,
-                         factsDistTo :: S.HashSet DistTo, factsEdge :: S.HashSet Edge}
+data DataBase = DataBase{factsStart :: S.HashSet String,
+                         factsEdge :: M.HashMap (String, String) (S.HashSet Dist),
+                         factsDistTo :: M.HashMap String (S.HashSet Dist)}
                   deriving (Show, Eq)
 
 emptyDB :: DataBase
-emptyDB = DataBase S.empty S.empty S.empty
+emptyDB = DataBase S.empty M.empty M.empty
 
 insertDB :: Fact -> DataBase -> (DataBase, Bool)
 insertDB fact db
@@ -75,99 +86,160 @@ insertDB fact db
                 (S.insert vnew hset', True)
       in
       case fact of
-          StartFact v -> first (\ hset -> db{factsStart = hset})
-                           (update (factsStart db) v)
-          DistToFact v -> first (\ hset -> db{factsDistTo = hset})
-                            (update (factsDistTo db) v)
-          EdgeFact v -> first (\ hset -> db{factsEdge = hset})
-                          (update (factsEdge db) v)
+          StartFact (Start v0) -> if S.member v0 (factsStart db) then
+                                    (db, False) else
+                                    (db{factsStart = S.insert v0 (factsStart db)}, True)
+          EdgeFact (Edge v0 v1 v2) -> if M.member (v0, v1) (factsEdge db)
+                                        then
+                                        first
+                                          (\ hset ->
+                                             db{factsEdge = M.insert (v0, v1) hset (factsEdge db)})
+                                          (update ((M.!) (factsEdge db) (v0, v1)) v2)
+                                        else
+                                        (db{factsEdge =
+                                              M.insert (v0, v1) (S.singleton v2) (factsEdge db)},
+                                         True)
+          DistToFact (DistTo v0 v1) -> if M.member v0 (factsDistTo db) then
+                                         first
+                                           (\ hset ->
+                                              db{factsDistTo = M.insert v0 hset (factsDistTo db)})
+                                           (update ((M.!) (factsDistTo db) v0) v1)
+                                         else
+                                         (db{factsDistTo =
+                                               M.insert v0 (S.singleton v1) (factsDistTo db)},
+                                          True)
 
-type Queue = Q.MaxQueue Fact
+type Queue = Q.MaxQueue Continuation
 
 step :: DataBase -> Fact -> Queue -> Queue
 step db fact q_1
   = case fact of
-        StartFact v_2 -> Q.unions
-                           [q_1,
-                            foldl'
-                              (\ q_3 (Start s0) ->
-                                 Q.unions [q_3, Q.singleton (DistToFact (DistTo s0 (DistNat 0)))])
-                              Q.empty
-                              ([v_2])]
-        DistToFact v_4 -> Q.unions
-                            [q_1,
-                             foldl'
-                               (\ q_5 (DistTo v10 d10) ->
-                                  Q.unions
-                                    [q_5,
-                                     foldl'
-                                       (\ q_7 (Edge v10_6 v20 d20) ->
-                                          Q.unions
-                                            [q_7,
-                                             Q.singleton (DistToFact (DistTo v20 (add d10 d20)))])
-                                       Q.empty
-                                       (foldl'
-                                          (\ rest (Edge v_8 v_9 v_10) ->
-                                             (pure Edge <*> mlbs v_8 v10 <*> pure v_9 <*> pure v_10)
-                                               ++ rest)
-                                          []
-                                          (S.toList (factsEdge db)))])
-                               Q.empty
-                               ([v_4])]
-        EdgeFact v_11 -> Q.unions
-                           [q_1,
-                            foldl'
-                              (\ q_12 (Edge v10 v20 d20) ->
-                                 Q.unions
-                                   [q_12,
+        StartFact (Start v_2) -> Q.unions
+                                   [q_1,
                                     foldl'
-                                      (\ q_14 (DistTo v10_13 d10) ->
-                                         Q.unions
-                                           [q_14,
-                                            Q.singleton (DistToFact (DistTo v20 (add d10 d20)))])
+                                      (\ q_3 (Start s0) ->
+                                         Q.unions [q_3, Q.singleton (InitCont s0)])
                                       Q.empty
-                                      (foldl'
-                                         (\ rest (DistTo v_15 v_16) ->
-                                            (pure DistTo <*> mlbs v_15 v10 <*> pure v_16) ++ rest)
-                                         []
-                                         (S.toList (factsDistTo db)))])
-                              Q.empty
-                              ([v_11])]
+                                      (S.foldl' (\ rest v_4 -> Start v_4 : rest) []
+                                         (S.singleton v_2))]
+        DistToFact (DistTo v_5 v_6) -> Q.unions
+                                         [q_1,
+                                          foldl'
+                                            (\ q_7 (DistTo v10 d10) ->
+                                               Q.unions
+                                                 [q_7,
+                                                  foldl'
+                                                    (\ q_9 (Edge v10_8 v20 d20) ->
+                                                       Q.unions
+                                                         [q_9,
+                                                          Q.singleton
+                                                            (AddDistCont v10_8 d10 v20 d20)])
+                                                    Q.empty
+                                                    (M.foldlWithKey'
+                                                       (\ rest (v_10, v_11) vals ->
+                                                          concatMap
+                                                            (\ v_12 ->
+                                                               pure Edge <*> mlbs v_10 v10 <*>
+                                                                 pure v_11
+                                                                 <*> pure v_12)
+                                                            vals
+                                                            ++ rest)
+                                                       []
+                                                       (factsEdge db))])
+                                            Q.empty
+                                            (M.foldlWithKey'
+                                               (\ rest v_13 vals ->
+                                                  S.foldl' (\ acc v_14 -> DistTo v_13 v_14 : acc) []
+                                                    vals
+                                                    ++ rest)
+                                               []
+                                               (M.singleton v_5 (S.singleton v_6)))]
+        EdgeFact (Edge v_15 v_16 v_17) -> Q.unions
+                                            [q_1,
+                                             foldl'
+                                               (\ q_18 (Edge v10 v20 d20) ->
+                                                  Q.unions
+                                                    [q_18,
+                                                     foldl'
+                                                       (\ q_20 (DistTo v10_19 d10) ->
+                                                          Q.unions
+                                                            [q_20,
+                                                             Q.singleton
+                                                               (AddDistCont v10_19 d10 v20 d20)])
+                                                       Q.empty
+                                                       (M.foldlWithKey'
+                                                          (\ rest v_21 vals ->
+                                                             concatMap
+                                                               (\ v_22 ->
+                                                                  pure DistTo <*> mlbs v_21 v10 <*>
+                                                                    pure v_22)
+                                                               vals
+                                                               ++ rest)
+                                                          []
+                                                          (factsDistTo db))])
+                                               Q.empty
+                                               (M.foldlWithKey'
+                                                  (\ rest (v_23, v_24) vals ->
+                                                     S.foldl'
+                                                       (\ acc v_25 -> Edge v_23 v_24 v_25 : acc)
+                                                       []
+                                                       vals
+                                                       ++ rest)
+                                                  []
+                                                  (M.singleton (v_15, v_16) (S.singleton v_17)))]
 
 enumDistTo :: DataBase -> [DistTo]
-enumDistTo db = S.toList (factsDistTo db)
+enumDistTo db
+  = M.foldlWithKey'
+      (\ rest v_28 vals ->
+         S.foldl' (\ acc v_29 -> DistTo v_28 v_29 : acc) [] vals ++ rest)
+      []
+      (factsDistTo db)
 
 closerThan :: Dist -> DataBase -> [DistTo]
-closerThan v_20 db
-  = foldl'
-      (\ rest (DistTo v_21 v_22) ->
-         (pure DistTo <*> pure v_21 <*> mlbs v_22 v_20) ++ rest)
+closerThan v_31 db
+  = M.foldlWithKey'
+      (\ rest v_32 vals ->
+         concatMap (\ v_33 -> pure DistTo <*> pure v_32 <*> mlbs v_33 v_31)
+           vals
+           ++ rest)
       []
-      (S.toList (factsDistTo db))
+      (factsDistTo db)
 
 distTo :: String -> DataBase -> [DistTo]
-distTo v_23 db
-  = foldl'
-      (\ rest (DistTo v_25 v_26) ->
-         (pure DistTo <*> mlbs v_25 v_23 <*> pure v_26) ++ rest)
+distTo v_34 db
+  = M.foldlWithKey'
+      (\ rest v_36 vals ->
+         concatMap (\ v_37 -> pure DistTo <*> mlbs v_36 v_34 <*> pure v_37)
+           vals
+           ++ rest)
       []
-      (S.toList (factsDistTo db))
+      (factsDistTo db)
 
 reachableIn :: String -> Dist -> DataBase -> [DistTo]
-reachableIn v_27 v_28 db
-  = foldl'
-      (\ rest (DistTo v_29 v_30) ->
-         (pure DistTo <*> mlbs v_29 v_27 <*> mlbs v_30 v_28) ++ rest)
+reachableIn v_38 v_39 db
+  = M.foldlWithKey'
+      (\ rest v_40 vals ->
+         concatMap
+           (\ v_41 -> pure DistTo <*> mlbs v_40 v_38 <*> mlbs v_41 v_39)
+           vals
+           ++ rest)
       []
-      (S.toList (factsDistTo db))
+      (factsDistTo db)
 
 compute :: [Fact] -> DataBase
-compute = go emptyDB . Q.fromList
+compute = go emptyDB . Q.fromList . map Initial
   where go :: DataBase -> Queue -> DataBase
         go db pq
           | Q.null pq = db
           | otherwise =
-            let (nextFact, pq') = Q.deleteFindMax pq
-                (db', changed) = insertDB nextFact db
-                pq'' = if changed then step db' nextFact pq' else pq'
+            let (nextFacts, pq') = first (evaluate db) $ Q.deleteFindMax pq
+                (db', pq'')
+                  = foldl'
+                      (\ (dbOld, pqOld) f ->
+                         let (dbNew, changed) = insertDB f dbOld
+                             pqNew = if changed then step dbNew f pqOld else pqOld
+                           in (dbNew, pqNew))
+                      (db, pq')
+                      nextFacts
               in go db' pq''
