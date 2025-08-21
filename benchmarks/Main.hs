ac6159e3e125fb751b9c32511a377d03ff6c346e
skcheongbrian <skcheongbrian@gmail.com>
2025-08-21 09:44:49 +0800

    add no priority kleen back

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -10,13 +10,13 @@ import ShortestPath.Converter
   ( loadGraph
   , convertToKleen
   , convertToHandwritten
-  -- , convertToNoPriorityKleen
+  , convertToNoPriorityKleen
   , Graph
   )
 import qualified ShortestPath.HandwrittenDijkstra as HW
 import qualified ShortestPath.ShortestPath as SP
 import qualified ShortestPath.SubOptHandwrittenDijkstra as SOHD
--- import qualified ShortestPath.ShortestPathNoPriority as SPNP
+import qualified ShortestPath.ShortestPathNoPriority as SPNP
 
 
 -- | Find all *.json graphs in a directory.
@@ -41,14 +41,14 @@ main = do
     let toBenchmarkGroups :: (String, Graph) -> Benchmark
         toBenchmarkGroups (name, g) =
             let facts = convertToKleen g
-                -- noPriorityFacts = convertToNoPriorityKleen g
+                noPriorityFacts = convertToNoPriorityKleen g
                 adj = convertToHandwritten g
             in 
             bgroup name 
                 [ bench "Kleen solve" $
                     nf SP.compute facts
-                -- , bench "Kleen (no priority) solve" $
-                --     nf SPNP.compute noPriorityFacts
+                , bench "Kleen (no priority) solve" $
+                    nf SPNP.compute noPriorityFacts
                 , bench "Handwritten Dijsktra solve" $
                       nf (SOHD.dijkstra "0") adj
             ]
