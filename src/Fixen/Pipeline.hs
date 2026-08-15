16f03673521294209cca74dc35e78afafd9836fc
SKCheongBrian <skcheongbrian@gmail.com>
2026-08-16 02:11:21 +0800

    Create pipeline that provides environmnent as well

--- src/Fixen/Pipeline.hs
+++ src/Fixen/Pipeline.hs
@@ -29,7 +29,7 @@
 --
 -- Some passes will emit warnings without causing the pipeline to terminate.
 -- These are automatically flushed to stderr (see 'runFixenPass').
-module Fixen.Pipeline (pipeline) where
+module Fixen.Pipeline (pipeline, pipelineWithSymbols) where
 
 import Control.Monad.IO.Class (MonadIO)
 import Data.IntMap.Strict qualified as Map
@@ -79,6 +79,34 @@ pipeline file_path contents error_printer = do
   (db, _) <- run st''' getRelationRepresentation
   (t, _) <- run st''' (codeGen rt db program')
   return (program', rt, db, t)
+  where
+    run :: WithErrors a => a -> FixenPass a b -> FixenM (b, a)
+    run = runFixenPass error_printer
+
+pipelineWithSymbols
+  :: FilePath
+  -> String
+  -> (forall m msg. (MonadIO m, Pretty msg) => Diagnostic msg -> m ())
+  -> FixenM
+      ( Program
+      , SymbolEnv
+      , NonEmpty RuleForest
+      , RelationRepresentation
+      , Text
+      )
+pipelineWithSymbols file_path contents error_printer = do
+  let file_map = [(file_path, contents)]
+      init_errs = emptyErrors file_map
+      init_pos_env = Map.empty
+      init_node_id = 0
+      init_env = init_pos_env × init_node_id × init_errs
+  (program, st) <- run init_env (parse file_path (pack contents))
+  (program', st') <- run st (getIncludes program)
+  (env, st'') <- run st' (solveSymbols program')
+  (rt, st''') <- run (env, st'') (getRuleForest program')
+  (db, _) <- run st''' getRelationRepresentation
+  (t, _) <- run st''' (codeGen rt db program')
+  return (program', env, rt, db, t)
   where
     run :: WithErrors a => a -> FixenPass a b -> FixenM (b, a)
     run = runFixenPass error_printer
