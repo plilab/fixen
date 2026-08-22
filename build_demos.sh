45944c7f216750e4bbcff065c8f95091340ce307
Foo Yong Qi <yongqifoo@gmail.com>
2026-08-22 22:28:20 +0800

    remove unused shell scripts

--- build_demos.sh
+++
@@ -1,6 +0,0 @@
-cabal run fixen -- -o benchmarks/ShortestPath/FixenNoPriorities.hs demos/ShortestPath/NoPriorities.fix
-cabal run fixen -- -o benchmarks/ShortestPath/FixenWithPriorities.hs demos/ShortestPath/WithPriorities.fix
-cabal run fixen -- -o benchmarks/Pareto/FixenNoPriorities.hs demos/Pareto/NoPriorities.fix
-cabal run fixen -- -o benchmarks/Pareto/FixenWithPriorities.hs demos/Pareto/WithPriorities.fix
-cabal run fixen -- -o benchmarks/ReducedProduct/FixenNoPriorities.hs demos/StaticAnalysis/ReducedProduct.fix
-cabal run fixen -- -o benchmarks/ReducedProduct/FixenWithPriorities.hs demos/StaticAnalysis/ReducedProductWithPriorities.fix
